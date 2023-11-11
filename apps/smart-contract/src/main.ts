901f1642755aa9a2da0fbcf732c8fc2e1895d849
popopolyakov <bagldya@gmail.com>
2023-11-11 18:27:23 +0300

    add deployment checker + env usage wallet choose

--- apps/smart-contract/src/main.ts
+++ apps/smart-contract/src/main.ts
@@ -1,39 +1,51 @@
 import { configDotenv } from 'dotenv';
 import { mnemonicToWalletKey } from '@ton/crypto';
-import { TonClient, WalletContractV4 } from '@ton/ton';
+import {
+  fromNano,
+  TonClient,
+  WalletContractV3R2,
+  WalletContractV4,
+} from '@ton/ton';
 import { getHttpEndpoint } from '@orbs-network/ton-access';
 import * as path from 'path';
-
-console.log(`Hello World ${process.cwd()}`);
 
 const envs = configDotenv({
   path: path.resolve(__dirname, '../.env.env'),
   debug: true,
 }).parsed;
 
-console.log(envs);
-
-const connectToWallet = async () => {
-  const mnemonic = envs.MNEMONIC_PHRASE;
+const connectToWallet = async (isReal = true) => {
+  const mnemonic = isReal ? envs.REAL_MNEMONIC_PHRASE : envs.MNEMONIC_PHRASE;
   const key = await mnemonicToWalletKey(mnemonic.split(' '));
 
-  const wallet = WalletContractV4.create({
+  console.log(mnemonic.split(' '));
+
+  const wallet = (isReal ? WalletContractV4 : WalletContractV3R2).create({
     publicKey: key.publicKey,
     workchain: 0,
   });
 
-  const endpoint = await getHttpEndpoint({ network: 'testnet' });
+  const endpoint = await getHttpEndpoint({
+    network: 'mainnet',
+  });
+
   const client = new TonClient({ endpoint });
 
   // const seqNo = await wallet.getSeqno();
 
+  console.log(wallet.address);
+
   const isContractDeployed = await client.isContractDeployed(wallet.address);
 
   if (!isContractDeployed) {
-    console.log('Is contract not deployed');
+    console.log('contract is not deployed');
   } else {
-    console.log('Is contract deployed');
+    console.log('contract deployed');
   }
+
+  const balance = await client.getBalance(wallet.address);
+
+  console.log({ balance: fromNano(balance) });
 };
 
 const main = async () => {
